7512aa65a5ca226c05a6c574a2123a362f2ca613
phstolpe <123520279+phstolpe@users.noreply.github.com>
2023-08-05 15:59:42 +0200

    add basic gui and listeners

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-//function that randoms a number between 0-2.
-// each number represents a choice in rock paper scissors
-// returns a string
-
 function getComputerChoice() {
   let choice = Math.floor(Math.random() * 3);
 
@@ -15,33 +11,45 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
   const parsedPlayerSelection = playerSelection.toLowerCase();
-
+  let result = "";
   if (parsedPlayerSelection === computerSelection){
-    return `Draw! ${parsedPlayerSelection} against ${computerSelection}`;
+    result = `Draw! ${parsedPlayerSelection} against ${computerSelection}`;
   }
   else if ((parsedPlayerSelection === "rock" && computerSelection === "scissors") || 
   (parsedPlayerSelection === "paper" && computerSelection === "rock") ||
   (parsedPlayerSelection === "scissors" && computerSelection === "paper")) {
-    return `You win! ${parsedPlayerSelection} beats ${computerSelection}`;
+    result = `You win! ${parsedPlayerSelection} beats ${computerSelection}`;
   }
   else {
-    return `You lose! ${computerSelection} beats ${parsedPlayerSelection}`;
+    result = `You lose! ${computerSelection} beats ${parsedPlayerSelection}`;
   }
+  display.textContent = result;
+  return result;
 }
 
-function game(){
-  let playerScore = 0;
-  let computerScore = 0;
-
-  for(let i=0; i<5; i++){
-    const result = playRound(prompt("rock, paper or scissors?"), getComputerChoice());
+function keepScore(result){
+    if (result[0]==="D")
+      return;
     if (result[4] === "w"){
       playerScore++;  
     }
     else if (result[4] === "l"){
       computerScore++;
     }
-    console.log(result + `. Player ${playerScore} - Computer ${computerScore}`);
-  }
+    scoreCard.textContent = `Player: ${playerScore} - Computer: ${computerScore}`;
 }
-game();
+
+let playerScore = 0;
+let computerScore = 0;
+const rock = document.querySelector("#rock-btn");
+const paper = document.querySelector("#paper-btn");
+const scissors = document.querySelector("#scissors-btn");
+const display = document.querySelector("#display");
+const scoreCard = document.querySelector("#score-card");
+
+rock.addEventListener("click", () => {
+  keepScore(playRound("rock", getComputerChoice()))});
+paper.addEventListener("click", () => {
+  keepScore(playRound("paper", getComputerChoice()))});
+scissors.addEventListener("click", () => {
+  keepScore(playRound("scissors", getComputerChoice()))});
